59643a423463316f8b9dba2ddbff257940a44413
jarokaz <jarokaz@no-reply.com>
2021-01-20 00:11:42 +0000

    Updates

--- vision/custom/trainer/task.py
+++ vision/custom/trainer/task.py
@@ -1,66 +1,39 @@
-# Single, Mirror and Multi-Machine Distributed Training for CIFAR-10
+# Custom Job for CIFAR10
 
 import tensorflow_datasets as tfds
 import tensorflow as tf
-from tensorflow.python.client import device_lib
+from hypertune import HyperTune
 import argparse
 import os
 import sys
-tfds.disable_progress_bar()
 
+# Command Line arguments
 parser = argparse.ArgumentParser()
+parser.add_argument('--job-dir')
 parser.add_argument('--model-dir', dest='model_dir',
-                    default=os.getenv("AIP_MODEL_DIR"), type=str, help='Model dir.')
+                    default=os.getenv('AIP_MODEL_DIR'), type=str, help='Model dir.')
 parser.add_argument('--lr', dest='lr',
-                    default=0.01, type=float,
+                    default=0.001, type=float,
                     help='Learning rate.')
-parser.add_argument('--epochs', dest='epochs',
-                    default=10, type=int,
-                    help='Number of epochs.')
-parser.add_argument('--steps', dest='steps',
-                    default=200, type=int,
-                    help='Number of steps per epoch.')
-parser.add_argument('--distribute', dest='distribute', type=str, default='single',
-                    help='distributed training strategy')
+parser.add_argument('--decay', dest='decay',
+                    default=0.01, type=float,
+                    help='Decay rate')
 args = parser.parse_args()
 
-print('Python Version = {}'.format(sys.version))
-print('TensorFlow Version = {}'.format(tf.__version__))
-print('TF_CONFIG = {}'.format(os.environ.get('TF_CONFIG', 'Not found')))
-print('DEVICES', device_lib.list_local_devices())
-
-# Single Machine, single compute device
-if args.distribute == 'single':
-    if tf.test.is_gpu_available():
-        strategy = tf.distribute.OneDeviceStrategy(device="/gpu:0")
-    else:
-        strategy = tf.distribute.OneDeviceStrategy(device="/cpu:0")
-# Single Machine, multiple compute device
-elif args.distribute == 'mirror':
-    strategy = tf.distribute.MirroredStrategy()
-# Multiple Machine, multiple compute device
-elif args.distribute == 'multi':
-    strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
-
-# Multi-worker configuration
-print('num_replicas_in_sync = {}'.format(strategy.num_replicas_in_sync))
-
-# Preparing dataset
-BUFFER_SIZE = 10000
-BATCH_SIZE = 64
-
-def make_datasets_unbatched():
-  # Scaling CIFAR10 data from (0, 255] to (0., 1.]
-  def scale(image, label):
+# Scaling CIFAR-10 data from (0, 255] to (0., 1.]
+def scale(image, label):
     image = tf.cast(image, tf.float32)
     image /= 255.0
     return image, label
 
-  datasets, info = tfds.load(name='cifar10',
-                            with_info=True,
-                            as_supervised=True)
-  return datasets['train'].map(scale).cache().shuffle(BUFFER_SIZE).repeat()
+# Download the dataset
+datasets = tfds.load(name='cifar10', as_supervised=True)
 
+# Preparing dataset
+BUFFER_SIZE = 10000
+BATCH_SIZE = 64
+train_dataset = datasets['train'].map(scale).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+test_dataset = datasets['test'].map(scale).batch(BATCH_SIZE)
 
 # Build the Keras model
 def build_and_compile_cnn_model():
@@ -74,21 +47,26 @@ def build_and_compile_cnn_model():
   ])
   model.compile(
       loss=tf.keras.losses.sparse_categorical_crossentropy,
-      optimizer=tf.keras.optimizers.SGD(learning_rate=args.lr),
+      optimizer=tf.keras.optimizers.SGD(learning_rate=args.lr, decay=args.decay),
       metrics=['accuracy'])
   return model
 
+model = build_and_compile_cnn_model()
+
+# Instantiate the HyperTune reporting object
+hpt = HyperTune()
+
+# Reporting callback
+class HPTCallback(tf.keras.callbacks.Callback):
+
+    def on_epoch_end(self, epoch, logs=None):
+        global hpt
+        hpt.report_hyperparameter_tuning_metric(
+        hyperparameter_metric_tag='val_accuracy',
+        metric_value=logs['val_accuracy'],
+        global_step=epoch)
+
 # Train the model
-NUM_WORKERS = strategy.num_replicas_in_sync
-# Here the batch size scales up by number of workers since
-# `tf.data.Dataset.batch` expects the global batch size.
-GLOBAL_BATCH_SIZE = BATCH_SIZE * NUM_WORKERS
-train_dataset = make_datasets_unbatched().batch(GLOBAL_BATCH_SIZE)
-
-with strategy.scope():
-  # Creation of dataset, and model building/compiling need to be within
-  # `strategy.scope()`.
-  model = build_and_compile_cnn_model()
-
-model.fit(x=train_dataset, epochs=args.epochs, steps_per_epoch=args.steps)
+model.fit(train_dataset, epochs=5, steps_per_epoch=10, validation_data=test_dataset.take(8),
+    callbacks=[HPTCallback()])
 model.save(args.model_dir)
